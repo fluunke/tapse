9b3cdc9c415b3b4b73e6ee4960d38de7699aa8ef
fluunke <flrk@tuta.io>
2022-04-29 17:29:25 +0200

    neater room creation

--- frontend/src/components/RoomBox.svelte
+++ frontend/src/components/RoomBox.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import type { Room } from "src/stores/room";
-
-  import { PlusIcon } from "svelte-feather-icons";
+  import { slide } from "svelte/transition";
 
   export let room_store: Room;
 
@@ -12,27 +11,10 @@
 <div class="relative pb-2" id="room_box">
   <div class="text-lg font-bold text-center">Rooms</div>
 
-  {#if room_creation_prompt}
-    <div
-      class="absolute flex flex-col items-center justify-center w-full h-full bg-white"
-    >
-      <input
-        class="w-40 p-1 rounded-t-md"
-        type="text"
-        placeholder="Room name:"
-        bind:value={room_name_input}
-        name="room_name"
-        id="room_name"
-      />
-      <button
-        on:click={() => room_store.create_room(room_name_input)}
-        class="w-40 p-1 text-white bg-blue-500 rounded-b-md">Create</button
-      >
-    </div>
-  {/if}
   <ul class="flex flex-wrap justify-center mb-2 space-x-3">
     {#each $room_store.rooms as room}
       <li
+        transition:slide={{ duration: 100 }}
         on:click={() => {
           room_store.set_current_room(room.id);
         }}
@@ -51,10 +33,47 @@
     {/each}
 
     <li
-      on:click={() => (room_creation_prompt = !room_creation_prompt)}
+      on:click|self={() => (room_creation_prompt = !room_creation_prompt)}
       class="flex p-1 transition rounded-md cursor-pointer children:self-center hover:bg-gray-200"
     >
-      <PlusIcon class="text-gray-700" />
+      {#if room_creation_prompt}
+        <div transition:slide={{ duration: 100 }} class="flex flex-row">
+          <input
+            bind:value={room_name_input}
+            class="p-1 border rounded-l-md"
+            type="text"
+            name="room_name"
+          />
+          <button
+            on:click={() => {
+              room_store.create_room(room_name_input);
+              room_creation_prompt = false;
+            }}
+            class="p-1 font-bold text-white bg-blue-500 hover:bg-blue-600 rounded-r-md"
+            >Create</button
+          >
+        </div>
+      {/if}
+      <svg
+        on:click={() => (room_creation_prompt = !room_creation_prompt)}
+        class:rotate-45={room_creation_prompt}
+        class="text-gray-700 transition transform"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        ><line x1="12" y1="5" x2="12" y2="19" /><line
+          x1="5"
+          y1="12"
+          x2="19"
+          y2="12"
+        /></svg
+      >
     </li>
   </ul>
 </div>
--- frontend/windi.config.ts
+++ frontend/windi.config.ts
@@ -5,4 +5,5 @@ export default defineConfig({
     extract: {
         include: ['src/**/*.{html,vue,jsx,tsx,svelte}'],
     },
+    safelist: 'rotate-45 rotate-0',
 })
