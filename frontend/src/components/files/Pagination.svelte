20e2e6c17738809b998a9577be8858ff5662f114
fluunke <flrk@tuta.io>
2022-04-30 18:37:48 +0200

    use the correct icon

--- frontend/src/components/files/Pagination.svelte
+++ frontend/src/components/files/Pagination.svelte
@@ -46,10 +46,7 @@
         stroke="currentColor"
         stroke-width="2"
         stroke-linecap="round"
-        stroke-linejoin="round"
-        ><polyline points="11 17 6 12 11 7" /><polyline
-          points="18 17 13 12 18 7"
-        /></svg
+        stroke-linejoin="round"><polyline points="15 18 9 12 15 6" /></svg
       ></button
     >
   </div>
